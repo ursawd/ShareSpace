21279e5f0b90c231b7691f0acf8151c2151db0ea
Phil Bailey <philbailey@msn.com>
2021-11-18 13:02:40 -0500

    start membership page, add swal

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$("#cart").on("click", function () {
+  sweet();
+});
+$("#bookbtn").on("click", function () {
+  sweet();
+});
+function sweet() {
+  swal("This fuctionality not implemented at this time");
+}
