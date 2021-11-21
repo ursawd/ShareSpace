ffdd60d61fad23b3b7b3a9c507b148e36bdd6d25
Phil Bailey <philbailey@msn.com>
2021-11-21 17:56:08 -0500

    milestone complete

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ $("#cart").on("click", function () {
 $("#bookbtn").on("click", function () {
   sweet();
 });
+$("#formbutton").on("click", function () {
+  sweet();
+});
 function sweet() {
   swal("This fuctionality not implemented at this time");
 }
